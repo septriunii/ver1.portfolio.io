9809b9fea330c713d76c4457ed0fab5c41c7439a
Anthony Alabado <anthonyalabado3712@gmail.com>
2023-08-29 23:23:16 +0800

    custom domain config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-// import million from "million/compiler";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    // million.vite({ auto: true }),
-    react(),
-  ],
+  plugins: [react()],
 });
