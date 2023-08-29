6249b1093414f72cd5326538b3084f6b8396b3c3
Anthony Alabado <anthonyalabado3712@gmail.com>
2023-08-29 22:56:57 +0800

    fixing

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/",
-  // base: "/ver1.portfolio.io/",
+  base: "/ver1.portfolio.io/",
 });
