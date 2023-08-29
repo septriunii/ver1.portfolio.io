c6448f4d7f4e3b80ae1204050f13e54f940f0eb5
Anthony Alabado <anthonyalabado3712@gmail.com>
2023-08-29 22:48:53 +0800

    latest

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-// import million from "million/compiler";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    // million.vite({ auto: true }),
-    react(),
-  ],
-  base: "/ver1.portfolio.io/",
+  plugins: [react()],
+  base: "/",
+  // base: "/ver1.portfolio.io/",
 });
