a2e975a44ddddb130b98c531ff03160fee6357fc
Anthony Alabado <anthonyalabado3712@gmail.com>
2023-09-11 12:22:14 +0800

    lazy loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, lazy, Suspense } from "react";
 import ToggleButton from "./components/ToggleButton";
-import {
-  Intro,
-  AboutMe,
-  Skills,
-  Projects,
-  Experience,
-  ContactMe,
-} from "./pages";
-import { Footer, Body } from "./components";
-import SummaryPage from "./components/SummaryPage";
 import Preloader from "./components/Preloader";
+import { Body } from "./components";
+import SummaryPage from "./components/SummaryPage";
+
+// Lazy load your custom components
+const LazyIntro = lazy(() => import("./pages/Intro"));
+const LazyAboutMe = lazy(() => import("./pages/AboutMe"));
+const LazySkills = lazy(() => import("./pages/Skills"));
+const LazyProjects = lazy(() => import("./pages/Projects"));
+const LazyExperience = lazy(() => import("./pages/Experience"));
+const LazyContactMe = lazy(() => import("./pages/ContactMe"));
+const LazyFooter = lazy(() => import("./components/Footer"));
 
 const App = () => {
   const [isActive, setIsActive] = useState(false);
@@ -40,13 +41,16 @@ const App = () => {
               <SummaryPage />
             ) : (
               <>
-                <Intro />
-                <AboutMe />
-                <Skills />
-                <Projects />
-                <Experience />
-                <ContactMe />
-                <Footer />
+                {/* Use Suspense to render the lazy-loaded components */}
+                <Suspense fallback={<div></div>}>
+                  <LazyIntro />
+                  <LazyAboutMe />
+                  <LazySkills />
+                  <LazyProjects />
+                  <LazyExperience />
+                  <LazyContactMe />
+                  <LazyFooter />
+                </Suspense>
               </>
             )}
           </Body>
